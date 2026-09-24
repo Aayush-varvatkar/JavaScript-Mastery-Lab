07465b72dcdd5df99be3aaaccc1818d26c06a236
Trevino20 <Varvatkaraayush@gmail.com>
2026-09-25 01:04:31 +0530

    Ways to define functions..

--- Types_Functions.js
+++ Types_Functions.js
@@ -19,9 +19,12 @@ let sayHello1 = () => {
 sayHello1();
 
 
-let sayHello2 = name => console.log(`hellow ${name}`);  
+let sayHello2 = name => console.log(`hellow ${name}`);   // no paranthesis needed for for single parameter
 sayHello2("bindok")
 
 
-let sayHello3 = (a,b)=> console.log(`hule luyya.. ${a}`)
+let sayHello3 = (a,b)=> console.log(`hule luyya.. ${a}`)  // single line can be written without {}
 sayHello3('Hule luyya')
+
+let add = (a,b) => a+b;  // instead of return statement.. written directly for single line
+console.log(add(5,2));
